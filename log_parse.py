fca1d543120a37bcafb25ea4f29586dbe7eec657
windann <32433519+windann@users.noreply.github.com>
2018-03-28 11:07:48 +0300

    Update log_parse.py

--- log_parse.py
+++ log_parse.py
@@ -3,21 +3,25 @@
 from collections import Counter
 import re
 
-
+# функция получения "чистых урлов" и запись всей информации (дата, тип запроса, сам урл) в словарь
 def urls_with_inf():
     url_inf_list = []
 
     with open('log.log', 'r') as f:
         # вся информация из файла, типы запросов, урлы, дата и т.д
         data = f.read().split('\n')
+        # шаблон даты
         date_pattern = r'\d{2}\/\w{3}\/\d{4}\s\d{2}:\d{2}:\d{2}'
+        # шаблон типа запроса
         type_request_pattern = r'"\w{3,7}'
+        # шаблон схемы
         pattern = r'http?\w:\/\/'
+        
         repl = 'http://'
         elem_url_inf = {}
 
         for elem in data:
-
+            # проверяем запрос ли это и записываем в словарь информацию о нём
             if re.search(date_pattern, elem) and re.search(type_request_pattern, elem) and re.search(pattern, elem):
                 elem_url_inf['date'] = re.findall(date_pattern, elem)[0]
                 elem_url_inf['request'] = re.findall(type_request_pattern, elem)[0].lstrip('"')
@@ -37,7 +41,7 @@ def urls_with_inf():
 
     return url_inf_list
 
-
+# возвращает список урлов, начиная с указанной даты
 def start_at_func(start_at, urls):
     when_start = 0
     urls_date = [elem['date'] + ' ' + elem['url'] for elem in urls_with_inf()]
@@ -50,7 +54,7 @@ def start_at_func(start_at, urls):
 
     return [url for url in urls[when_start:]]
 
-
+# возвращает список урлов, заканчивая указанной датой
 def stop_at_func(stop_at, urls):
     urls_date = [elem['date'] + ' ' + elem['url'] for elem in urls_with_inf()]
 
@@ -62,14 +66,15 @@ def stop_at_func(stop_at, urls):
 
     return [url for url in urls[:when_finish]]
 
-
+# проходим по урлам в обратном порядке, если встречаем среди последних 4ёх символов точку, занчит это файл
+# урл должен быть удалён
 def ignore_files_func(urls):
     for url in urls:
-        for i in range(len(url) - 1, len(url) - 4, -1):
+        for i in range(len(url) - 1, len(url) - 5, -1):
             if url[i] == '.':
                 urls.remove(url)
 
-
+# прверяем наличие текущего урла в списке запрещённых
 def ignore_urls_func(urls,ignore_urls):
     for ignore_url in ignore_urls:
         for url in urls:
@@ -78,28 +83,36 @@ def ignore_urls_func(urls,ignore_urls):
 
 
 def slow_queries_func():
+    # запись в список кортежей время и запрос
     urls_time = [(int(elem['time']), elem['url']) for elem in urls_with_inf()]
+    # сортировка по времени, начиная с самых медленных
     urls_time.sort(key=lambda x: x[0], reverse=True)
 
     slow_time_list = []
     slow_list = []
-
+    # если текущий урл в списке самых медленных(после сортировки первые 5 элементов - самые медленные)
+    # записываем все подходящие урлы в единый словарь
+    # а в другой список записываем ещё и информацию о времени
     for elem_with_time in urls_time:
         for elem in urls_time[:5]:
             if elem_with_time[1] == elem[1]:
                 slow_time_list.append(elem_with_time[1])
                 slow_list.append(elem_with_time)
-
+                
+            
+    # находим количество повторений таких урлов 
     c_s = Counter(slow_time_list)
     avg_list = []
-
+    # проходим по словарю, где указаны количества повторений и по общему списку
+    # в случае совпадения считаем всю сумму времени
+    # затем из словаря берём количество вхождений и находим среднее
     for elem_k, elem_v in c_s.items():
         s_s = 0
         for slow_elem in slow_list:
             if elem_k == slow_elem[1]:
                 s_s += slow_elem[0]
         avg_list.append(s_s // elem_v)
-
+    # отсортированный список переворачиваем
     return sorted(avg_list)[::-1]
 
 
